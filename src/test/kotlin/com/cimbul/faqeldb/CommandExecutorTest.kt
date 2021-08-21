f26e3f2138c75d079155ed5b8bf3320bece8a3ae
Tim Yates <cimbul@gmail.com>
2021-08-21 07:28:07 -0500

    Test static queries

--- src/test/kotlin/com/cimbul/faqeldb/CommandExecutorTest.kt
+++ src/test/kotlin/com/cimbul/faqeldb/CommandExecutorTest.kt
@@ -28,5 +28,59 @@ class CommandExecutorTest : DescribeSpec({
 
             result.firstPage().values().single() shouldBe ionTextValue("5")
         }
+
+        it("should support queries on static data") {
+            val request = ExecuteStatementRequest.builder().build {
+                statement("""
+                    SELECT e.name AS employeeName,
+                           e.projects[0].name AS firstProjectName
+                    FROM
+                        {
+                             'employeesNest': <<
+                                 {
+                                  'id': 3,
+                                  'name': 'Bob Smith',
+                                  'title': null,
+                                  'projects': [ { 'name': 'AWS Redshift Spectrum querying' },
+                                                { 'name': 'AWS Redshift security' },
+                                                { 'name': 'AWS Aurora security' }
+                                              ]
+                                  },
+                                  {
+                                      'id': 4,
+                                      'name': 'Susan Smith',
+                                      'title': 'Dev Mgr',
+                                      'projects': []
+                                  },
+                                  {
+                                      'id': 6,
+                                      'name': 'Jane Smith',
+                                      'title': 'Software Eng 2',
+                                      'projects': [ { 'name': 'AWS Redshift security' } ]
+                                  }
+                             >>
+                        } AS hr,
+                        hr.employeesNest AS e
+                """)
+            }
+
+            val result = executor.executeStatement(request)
+
+            result.firstPage().values().single().ionElement() shouldBe ionElement("""
+                [
+                    {
+                      "employeeName": "Bob Smith",
+                      "firstProjectName": "AWS Redshift Spectrum querying"
+                    },
+                    {
+                      "employeeName": "Susan Smith"
+                    },
+                    {
+                      "employeeName": "Jane Smith",
+                      "firstProjectName": "AWS Redshift security"
+                    }
+                ]
+            """)
+        }
     }
 })
