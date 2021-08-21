e5a3b4b067203ae8cbfdd83a8bd668820b270588
Tim Yates <cimbul@gmail.com>
2021-08-21 13:26:02 -0500

    Add no-op visitor transform

--- src/main/kotlin/com/cimbul/faqeldb/Evaluator.kt
+++ src/main/kotlin/com/cimbul/faqeldb/Evaluator.kt
@@ -5,13 +5,23 @@ import com.amazon.ionelement.api.IonElement
 import com.amazon.ionelement.api.toIonElement
 import com.amazon.ionelement.api.toIonValue
 import org.partiql.lang.CompilerPipeline
+import org.partiql.lang.ast.toAstStatement
+import org.partiql.lang.ast.toExprNode
+import org.partiql.lang.domains.PartiqlAst
 import org.partiql.lang.eval.EvaluationSession
 import org.partiql.lang.eval.ExprValueFactory
 
 class Evaluator {
     private val ion = IonSystemBuilder.standard().build()
     private val valueFactory = ExprValueFactory.standard(ion)
-    private val compiler = CompilerPipeline.standard(valueFactory)
+    private val transform = PartiqlAst.VisitorTransform()
+    private val compiler = CompilerPipeline.build(valueFactory) {
+        addPreprocessingStep { exprNode, _ ->
+            val preTransform = exprNode.toAstStatement()
+            val postTransform = transform.transformStatement(preTransform)
+            postTransform.toExprNode(ion)
+        }
+    }
 
     fun evaluate(statement: String, parameters: List<IonElement>): AnyElement {
         val expression = compiler.compile(statement)
