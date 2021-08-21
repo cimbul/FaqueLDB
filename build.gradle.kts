891d279dbee9631ce9190f4e73368ef952fbd564
Tim Yates <cimbul@gmail.com>
2021-08-21 12:16:13 -0500

    Add server

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,14 @@ tasks.withType<Test>() {
 dependencies {
     implementation("org.partiql:partiql-lang-kotlin:0.3.1")
 
+    implementation(platform("org.http4k:http4k-bom:4.11.0.1"))
+    implementation("org.http4k:http4k-core")
+    implementation("org.http4k:http4k-server-netty")
+
+    val jacksonVersion = "2.12.4"
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+
     testImplementation("io.kotest:kotest-runner-junit5:4.6.1")
     testImplementation("io.kotest:kotest-assertions-core:4.0.7")
 }
